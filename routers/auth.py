076bb0098b7569d3644c9f34a652ebe072dab0b5
Lais <laiscristina.animamundi@gmail.com>
2026-01-15 19:00:15 -0300

    admin feito

--- routers/auth.py
+++ routers/auth.py
@@ -13,23 +13,6 @@ def get_db():
     finally:
         db.close()
 
-@router.post("/cadastro")
-def cadastro(dados: dict, db: Session = Depends(get_db)):
-    if db.query(Usuario).filter_by(email=dados["email"]).first():
-        raise HTTPException(status_code=400, detail="Email já cadastrado")
-
-    usuario = Usuario(
-        nome=dados["nome"],
-        email=dados["email"],
-        senha=hash_senha(dados["senha"]),
-        perfil="admin"
-    )
-
-    db.add(usuario)
-    db.commit()
-
-    return {"msg": "Usuário cadastrado"}
-
 @router.post("/login")
 def login(dados: dict, db: Session = Depends(get_db)):
     usuario = db.query(Usuario).filter_by(
@@ -40,6 +23,8 @@ def login(dados: dict, db: Session = Depends(get_db)):
     if not usuario:
         raise HTTPException(status_code=401, detail="Login inválido")
 
-    return {"access_token": criar_token({"sub": usuario.email})}
-
-
+    return {
+        "access_token": criar_token({"sub": usuario.email}),
+        "perfil": usuario.perfil,
+        "nome": usuario.nome
+    }
