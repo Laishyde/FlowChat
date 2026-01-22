2c08588e2328f5de08599220d2c117ee1c3bb418
Lais <laiscristina.animamundi@gmail.com>
2026-01-21 21:10:18 -0300

    Configura DATABASE_URL

--- database.py
+++ database.py
@@ -1,13 +1,18 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./meur.db"
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    pool_pre_ping=True
 )
 
-SessionLocal = sessionmaker(bind=engine)
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
